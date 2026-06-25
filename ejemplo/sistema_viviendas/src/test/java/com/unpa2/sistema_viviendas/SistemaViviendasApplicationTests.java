053ef2a98304ca3f4ecba14a2e1fbc59967e6c21
FSNCHZ <snchzfrancisco28@gmail.com>
2026-06-24 21:35:54 -0300

    Actualización Video Entrega

---
+++ ejemplo/sistema_viviendas/src/test/java/com/unpa2/sistema_viviendas/SistemaViviendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unpa2.sistema_viviendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaViviendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
